463d6cf876281234bdc874dd0e1ed1be5f5a0dc3
Maden5126 <madenon123@gmail.com>
2021-10-08 10:53:33 +0300

    eslint updated

--- src/js/Character.js
+++ src/js/Character.js
@@ -1,5 +1,6 @@
 export default class Character {
   static types = ['Bowman', 'Swordsman', 'Magician', 'Daemon', 'Undead', 'Zombie'];
+
   constructor(name, type) {
     if (typeof name !== 'string' || name.length < 2 || name.length > 10) {
       throw new Error('Имя должно быть от 2 до 10 символов');
