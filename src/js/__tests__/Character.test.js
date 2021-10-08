b55dbd32d92666af3e3cb7dd74cbaf046ab5c3de
Maden5126 <madenon123@gmail.com>
2021-10-08 10:57:34 +0300

    tests fixed

--- src/js/__tests__/Character.test.js
+++ src/js/__tests__/Character.test.js
@@ -8,16 +8,3 @@ test('should throw error for name less than 2 symbols', () => {
 test('should throw error for unknown type', () => {
   expect(() => { new Character('Oleg', 'someone'); }).toThrow('неверный класс');
 });
-
-test('should create a Character', () => {
-  const newPlayer = new Character('Oleg', 'Bowman', 25, 25);
-  const expected = {
-    name: 'Oleg',
-    type: 'Bowman',
-    health: 100,
-    level: 1,
-    attack: 25,
-    defence: 25,
-  };
-  expect(newPlayer).toEqual(expected);
-});
