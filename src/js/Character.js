8bf5f6b62f1084d5d7154c4037ee7687c0de9d9b
Maden5126 <madenon123@gmail.com>
2021-10-08 10:49:54 +0300

    character.js updated

--- src/js/Character.js
+++ src/js/Character.js
@@ -1,17 +1,17 @@
 export default class Character {
   static types = ['Bowman', 'Swordsman', 'Magician', 'Daemon', 'Undead', 'Zombie'];
-  constructor(name, type, attack, defence) {
+  constructor(name, type) {
     if (typeof name !== 'string' || name.length < 2 || name.length > 10) {
       throw new Error('Имя должно быть от 2 до 10 символов');
+    } else {
+      this.name = name;
     }
     if (!(Character.types.includes(type))) {
       throw new Error('неверный класс');
+    } else {
+      this.type = type;
     }
-    this.name = name;
-    this.type = type;
     this.health = 100;
     this.level = 1;
-    this.attack = attack;
-    this.defence = defence;
   }
 }
